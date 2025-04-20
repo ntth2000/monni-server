f0837c0b117a2b26a824871273498bbd1264ac38
ntth2000 <thaihuyennguyen2000@gmail.com>
2025-04-20 17:20:02 +0700

    initialize monni-server

---
+++ src/main/java/com/monniserver/monniserver/MonniserverApplication.java
@@ -0,0 +1,13 @@
+package com.monniserver.monniserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonniserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonniserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monniserver/monniserver/MonniserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monniserver.monniserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonniserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
